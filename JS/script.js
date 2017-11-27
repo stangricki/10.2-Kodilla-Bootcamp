a9c5bb81981650b86c910bc6030b5a9a947c0cd3
Filip Stangricki <stangricki@gmail.com>
2017-11-27 17:40:12 +0100

    jQuery - 10.2 assignment

---
+++ JS/script.js
@@ -0,0 +1,14 @@
+$('span').each(function(index, element) {
+	if (index % 2 == 0) {
+		$(element).css('color', 'red')
+	}
+});
+
+$('p').each(function(index, element) {
+	var button = '<button class="btn" data-tmp="' + index + '">Click me </button>'
+	$(element).append(button)
+});
+
+$('button').click(function() {
+	alert($(this).attr("data-tmp"));
+});
